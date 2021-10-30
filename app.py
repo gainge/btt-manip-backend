78f3a244403be51f12b226082e18864cdd67d7e0
Gibson Ainge <gainge@gmail.com>
2021-10-30 04:49:00 -0600

    add some basic validation to request arg

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import re
 from flask import Flask, render_template, request
 import logging
 from flask.json import jsonify
@@ -16,8 +17,28 @@ def index():
 
 @app.route('/seed', methods=['GET'])
 def getSeed():
-    characterSequence = request.args.getlist('seq[]')
-    # TODO: validate this list lol
+    rawSeq = request.args.getlist('seq[]')
+    
+    # Validate length
+    if len(rawSeq) < 1:
+        return 'Invalid sequence length!', 403
+
+    # Validate integer contents
+    characterSequence = []
+    try:
+        characterSequence = [int(x) for x in rawSeq]
+    except ValueError:
+        logging.error('Error parsing character sequence')
+        return 'Invalid character ID(s)', 403
+    except Exception as e:
+        logging.error(e)
+        return 
+    
+    # Check domain of response
+    if not all(x in range(0,25) for x in characterSequence):
+        logging.error('Specified character IDs outside valid range!')
+        return 'Invalid character ID(s)', 403
+
     seed = api.findSeed(characterSequence)
     result = {'seed': seed}
     return jsonify(result)
